8979a16439823225b37ef4fa9a46bdf0f0004dff
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-21 18:04:09 +0600

    Added sending Referrer header

--- api/axios.js
+++ api/axios.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
 const https = require('https');
 
-const { serverHost, serverPort } = config;
+const { host, port, serverHost, serverPort } = config;
 
 const instance = axios.create({
-  baseURL: `${serverHost}:${serverPort}/api`,
+  baseURL: `http://${serverHost}:${serverPort}/api`,
   httpsAgent: new https.Agent({
     rejectUnauthorized: false,
   }),
 });
+
+instance.defaults.headers.common['Referrer'] = `${host}:${port}`;
 
 module.exports = instance;
