efc27a0631f21b2c597c44c2e31b36a79662fa59
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-22 01:37:47 +0600

    Added /health endpoint

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const makeBuild = require('../controllers/makeBuild');
+const checkHealth = require('../controllers/checkHealth');
 
 router.post('/build', makeBuild);
+router.get('/health', checkHealth);
 
 module.exports = router;
